fixed Descriminate Mapping's Q calculation

bag_representation/mapping.py
@@ -188,9 +188,7 @@ class DiscriminativeMapping(MILESBase):
         Y = np.array([i * j for i in label for j in label]).reshape(len(label), len(label))
         B, A = np.unique(Y, return_counts=True)[1]
 
-        # they switched A and B values. A corresponds to matches and should be positive. B corresponds to non-matches and should be negative
-        Q = np.where(Y == 1, 1 / A, -1 / B)
-        # Q = np.where(Y==-1, -1/A, 1/B)
+        Q = np.where(Y == 1, -1 / A, 1 / B)
 
         # calculate J
         J = np.sum(ins_bag @ Q, axis=1)